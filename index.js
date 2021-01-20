6933255fe7740f5eecb30b6be5960768007e7da5
gallerdude <gallerdude@gmail.com>
2021-01-19 18:29:23 -0600

    manage state in one object instead of two lists

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ var fishTokens =  Array.from(document.getElementsByClassName("fishtoken"));
 
 var generateButton = document.getElementById("generateButton")
 
+var board = []
 
 var attempt = 0;
 
@@ -60,19 +61,27 @@ const generateNewBoard = () => {
     let resourceOptions = ["sheep", "sheep", "sheep", "sheep", "wood", "wood", "wood", "wood", "wheat", "wheat", "wheat", "wheat", "brick", "brick", "brick", "rock", "rock", "rock", "desert"]
     shuffleArray(resourceOptions)
 
+    board = resourceOptions.map(r => {
+        return {
+            "resource": r
+        }
+    });
+
     if (greatCaravanEnabled) { //Swap desert with center square
-        const desertIndex = resourceOptions.findIndex(x => x == "desert");
-        const temp = resourceOptions[9];
+        const desertIndex = board.findIndex(x => x.resource === "desert");
+        const temp = board[9];
     
-        resourceOptions[9] = "desert";
-        resourceOptions[desertIndex] = temp;
+        board[9] = {
+            "resource": "desert"
+        };
+        board[desertIndex] = temp;
     }
 
-    resourceOptions.forEach((resource, index) => {
-        if (resource === "desert" && greatCaravanEnabled) {
+    board.forEach((tile, index) => {
+        if (tile.resource === "desert" && greatCaravanEnabled) {
             spots[index].src = pickRandomly("images/caravan1.jpg", "images/caravan2.jpg")
         } else {
-            spots[index].src = `images/${resource}.png`
+            spots[index].src = `images/${tile.resource}.png`
         }
     });
 
@@ -82,16 +91,27 @@ const generateNewBoard = () => {
     let counts = [2, 3, 3, 4, 4, 5, 5, 6, 6, 8, 8, 9, 9, 10, 10, 11, 11, 12]
     shuffleArray(counts);
 
-    counts.splice(resourceOptions.findIndex(x => x === "desert"), 0, -1) //we need to add a count in for the desert
+    counts.splice(board.findIndex(x => x.resource === "desert"), 0, -1) //we need to add a count in for the desert
 
-    resourceOptions.forEach((resource, index) => {
+    console.log(board);
+
+    board = board.map((tile, index) => {
+        return {
+            ...tile,
+            count: counts[index]
+        }
+    });
+
+    console.log(board)
+
+    board.forEach(({resource, count}, index) => {
         if (resource !== "desert") {
             dots[index].style.top = `${spots[index].offsetTop + 40}px`
             dots[index].style.left = `${spots[index].offsetLeft + 70}px`
 
             dots[index].style.zIndex = 5;
 
-            dots[index].innerHTML = `${counts[index]}<br>${probabilities[counts[index]]}`
+            dots[index].innerHTML = `${count}<br>${probabilities[count]}`
 
             if (counts[index] === 6 || counts[index] === 8) {
                 dots[index].style.color = "red";
@@ -108,6 +128,8 @@ const generateNewBoard = () => {
     //GET DISTRIBUTION OF RESOURCES
     console.log("Get and view distribution of resources");
 
+    let totalCount = 0;
+
     const totalResourceCounts = {
         wood: 0,
         brick: 0,
@@ -116,17 +138,12 @@ const generateNewBoard = () => {
         rock: 0
     }
 
-    resourceOptions.forEach((resourceOption, index) => {
-        if (resourceOption !== "desert") {
-            totalResourceCounts[resourceOption] += probabilities[counts[index].toString()].length;
-        }
-    })
+    board.forEach(({ resource, count }, index) => {
+        if (resource !== "desert") {
+            const dotCount = probabilities[count.toString()].length;
 
-    let totalCount = 0;
-
-    counts.forEach((count) => {
-        if (count > 0) {
-            totalCount = totalCount + probabilities[count.toString()].length
+            totalResourceCounts[resource] = totalResourceCounts[resource] + dotCount;
+            totalCount = totalCount + dotCount;
         }
     })
 
@@ -203,24 +220,20 @@ const generateNewBoard = () => {
             "wood": 0,
         }
 
-        counts.forEach((count, index) => {
+        board.counts.forEach(({ resource, count }) => {
             if (isRedSpace(count)) {
-                redSpaceResourceTypes[resourceOptions[index]] = redSpaceResourceTypes[resourceOptions[index]] + 1;
+                redSpaceResourceTypes[resource] = redSpaceResourceTypes[resource] + 1;
             }
         })
 
-        Object.values(redSpaceResourceTypes).forEach((redSpaceCount) => {
-            if (redSpaceCount > 1) {
-                boardDefect = true;
-            }
-        })
+        if (Object.values(redSpaceResourceTypes).some(x => x > 1)) boardDefect = true;
 
     }
     if (noNeighboringRedSpaces) {
-        counts.forEach((count, index) => {
+        board.forEach(({ count }, index) => {
             if (isRedSpace(count)) {
                 spotAdjacencyTable[index].forEach((adjacentSpot) => {
-                    if (isRedSpace(counts[adjacentSpot])) {
+                    if (isRedSpace(board[adjacentSpot].count)) {
                         boardDefect = true;
                     }
                 });
@@ -229,8 +242,8 @@ const generateNewBoard = () => {
 
     }
     if (noResourceClusters) {
-        resourceOptions.forEach((resource, index) => {
-            const neighboringResources = spotAdjacencyTable[index].map((x) => resourceOptions[x]).filter((y) => y === resource);
+        board.forEach(({ resource }, index) => {
+            const neighboringResources = spotAdjacencyTable[index].map((x) => board[x].resource).filter((y) => y === resource);
             const matchingNeighborsCount = neighboringResources.length;
             
             if (resource === "wood" || resource === "sheep" || resource === "wheat") {
@@ -256,7 +269,7 @@ const generateNewBoard = () => {
         });
     }
     if (fishermenOfCatanEnabled) {
-        const desertIndex = resourceOptions.findIndex(x => x === "desert");
+        const desertIndex = board.findIndex(x => x.resource === "desert");
 
         if (!(desertIndex === 4 || desertIndex === 5 || desertIndex === 8 || desertIndex === 9 || desertIndex === 10 || desertIndex === 13 || desertIndex === 14)) {
             boardDefect = true;
